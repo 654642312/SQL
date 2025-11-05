572388ed2ec206dcdffec515d41490cce5c75998
654642312 <edgar_belandria010@hotmail.com>
2025-11-05 09:47:04 -0400

    feat: create offer function

--- auction-system/functions.sql
+++ auction-system/functions.sql
@@ -13,3 +13,33 @@ BEGIN
     RETURN v_current_balance - v_pending_spend
 END;
 $$ LANGUAGE pspgsql;
+
+CREATE OR REPLACE FUNCTION create_offer(p_user_id INTEGER, p_product_id INTEGER, p_offer_amount, DECIMAL)
+RETURN offer AS $$
+DECLARE 
+    v_maximum_offer_value
+    v_block_balance_table
+    v_real_balance
+BEGIN
+    SELECT MAX(amount) INTO v_maximum_offer_value FROM offer
+    WHERE product_id = p_product_id
+    FOR UPDATE;
+
+    IF p_offer_amount > v_maximum_offer_value THEN
+    RAISE EXCEPTION 'TOO LOW OFFER %.2f must be higher than the current offer %.2f.' p_offer_amount, v_maximum_offer_value;
+
+    SELECT amount INTO v_block_balance_table
+    FROM balance
+    WHERE user_id p_user_id;
+    FOR UPDATE
+
+    v_real_balance := get_available_balance(p_user_id);
+
+    IF v_real_balance >= p_offer_amount THEN RAISE EXCEPTION
+    'INSUFFICIENT FUNDS: only %.2f available. The offer is %.2f' v_real_balance, p_offer_amount
+
+    INSERT INTO offer(user_id, product_id, amount)
+    VALUES(p_user_id, p_product_id, p_offer_amount);
+END;
+$$ LANGUAGE pspgsql;
+
