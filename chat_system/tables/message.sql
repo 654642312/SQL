e1d92972fa2926f750e5097fc47eba3cfa0d5906
654642312 <edgar_belandria010@hotmail.com>
2025-12-17 19:40:22 -0400

    feat: add count_delivered, coun_read and total_receivers in the message table

--- chat_system/tables/message.sql
+++ chat_system/tables/message.sql
@@ -4,5 +4,8 @@ CREATE TABLE message (
     sender_id INT NOT NULL REFERENCES users(id),
     content VARCHAR(500) NOT NULL,
     parent_message_id INT REFERENCES messages(id) ON DELETE SET NULL,
+    count_delivered INT NOT NULL DEFAULT 0,
+    count_read INT NOT NULL DEFAULT 0,
+    total_receivers INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
